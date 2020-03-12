ad2382a43a1538c490223590b3dbdaacf761809f
SunShuHai <18842641842@163.com>
2020-03-12 14:14:02 +0800

    Update sh

--- arc.sh
+++ arc.sh
@@ -1,44 +1,102 @@
-#!/bin/bash
+##!/bin/bash
+#
+#projectName="GitTest"
+#
+#targetName="GitTest"
+#
+#codeSign="iPhone Developer"
+#
+#macPassword="1202"
+#
+#infoPlistPath="./${targetName}/Info.plist"
+#
+#exportOptionsPath="./ExportOptions.plist"
+#
+#if test ${targetName} = "GitTest"; then
+#
+#infoPlistPath="./${targetName}/Info.plist"
+#
+#elif test ${targetName} = "GitTest2"; then
+#
+#infoPlistPath="./GitTest2.plist"
+#
+#elif test ${targetName} = "GitTest3"; then
+#
+#infoPlistPath="./GitTest3.plist"
+#
+#fi
+#
+#bundleShortVersion=$(/usr/libexec/PlistBuddy -c "print CFBundleShortVersionString" "${infoPlistPath}")
+#
+#bundleVersion=$(/usr/libexec/PlistBuddy -c "print CFBundleVersion" "${infoPlistPath}")
+#
+#if test ${bundleShortVersion} = "\$(MARKETING_VERSION)" ; then
+#  bundleShortVersion=$(xcodebuild -workspace "${projectName}.xcworkspace" -scheme "${targetName}" -showBuildSettings | grep MARKETING_VERSION | tr -d 'MARKETING_VERSION =')
+#fi
+#
+#if test ${bundleVersion} = "\$(CURRENT_PROJECT_VERSION)" ; then
+#  bundleVersion=$(xcodebuild -workspace "${projectName}.xcworkspace" -scheme "${targetName}" -showBuildSettings | grep CURRENT_PROJECT_VERSION | tr -d 'CURRENT_PROJECT_VERSION =')
+#fi
+#
+#DATE="$(date +%Y_%m_%d_%H_%M_%S)"
+#
+#echo ""
+#echo "start build project: ${projectName}, scheme: ${targetName}, version: ${bundleShortVersion}, build: ${bundleVersion}"
+#echo ""
+#
+#IPANAME="${targetName}_V${bundleShortVersion}(${bundleVersion}).ipa"
+#
+#xcodebuild clean -workspace "${projectName}.xcworkspace" -scheme "${targetName}" -configuration 'Release'  -quiet
+#
+##xcodebuild build -workspace "${projectName}.xcworkspace" -scheme "${targetName}" -sdk iphoneos -configuration 'Release' CODE_SIGN_IDENTITY="${codeSign}" SYMROOT='$(PWD)' method="app-store" -quiet
+#
+#dirName="${targetName}_V${bundleShortVersion}_${DATE}"
+#
+#dirPath="~/Desktop/${dirName}"
+#
+#archivePath="${dirPath}/${targetName}"
+#
+#security unlock-keychain -p "${macPassword}" $keychainPath
+#
+#echo ""
+#echo "archive to path ${dirPath}/${targetName}.xcarchive"
+#echo ""
+#
+#xcodebuild archive -workspace "${projectName}.xcworkspace" -scheme "${targetName}" -configuration 'Release' -archivePath "${archivePath}" -quiet
+#
+#if [ $? -eq 0 ]
+#then
+#
+#echo ""
+#echo "exportArchive to path ${dirPath}/${targetName}.ipa"
+#echo ""
+#
+#xcodebuild -exportArchive -archivePath "${archivePath}.xcarchive" -exportPath "${dirPath}"  PROVISIONING_PROFILE_SPECIFIER ${codeSign} AppStore -exportOptionsPlist "${exportOptionsPath}" -quiet
+#
+#fi
+#
+#IC_USER="gavin.sun@mingdao.com"
+#IC_PASSWORD="Ssh.0411"
+#
+#altool="/Applications/ApplicationLoader.app/Contents/Frameworks/ITunesSoftwareService.framework/Versions/A/Support/altool"
+#
+#IPAPATH="${dirPath}/${targetName}.ipa"
+#
+#echo ""
+#echo "validate archive"
+#echo ""
+#
+#${altool} -v -f "${IPAPATH}" -u $IC_USER -p $IC_PASSWORD
+#
+#if [ $? -eq 0 ]
+#then
+#
+#echo ""
+#echo "upload archive"
+#echo ""
+#${altool} --upload-app -f "${IPAPATH}" -u $IC_USER -p $IC_PASSWORD
+#
+#fi
+#
 
-projectName="GitTest"
 
-targetName="GitTest"
- 
-codeSign="iPhone Developer"
-
-infoPlistPath="./${projectName}/Info.plist"
-
-bundleShortVersion=$(/usr/libexec/PlistBuddy -c "print CFBundleShortVersionString" "${infoPlistPath}")
-
-bundleVersion=$(/usr/libexec/PlistBuddy -c "print CFBundleVersion" "${infoPlistPath}")
-
-DATE="$(date +%Y%m%d%H%M%S)"
-
-IPANAME="${targetName}_V${bundleShortVersion}_${DATE}.ipa"
-
-xcodebuild -workspace "${projectName}.xcworkspace" -scheme "${targetName}" -configuration 'Release' clean
-
-xcodebuild -workspace "${projectName}.xcworkspace" -scheme "${targetName}" -sdk iphoneos -configuration 'Release' CODE_SIGN_IDENTITY="${codeSign}" SYMROOT='$(PWD)'
-
-if [ $? -eq 0 ]
-then
-    dirName="${targetName}_V${bundleShortVersion}_${DATE}"
-    mkdir "${dirName}"
-
-    IPA_PATH="${PWD}/${dirName}/${IPANAME}"
-
-    #xcrun -sdk iphoneos PackageApplication "./Release-iphoneos/${targetName}.app" -o ~/"${IPANAME}"
-    xcrun -sdk iphoneos PackageApplication "./Release-iphoneos/${targetName}.app" -o "${IPA_PATH}"
-
-    #移动dsym文件到dir下
-    mv -i "./Release-iphoneos/${targetName}.app.dSYM" "${dirName}"
-    mv -i "./${dirName}" ~/Desktop
-    rm -r ./Release-iphoneos
-
-    echo "ipa path: ~/Desktop/${dirName}"
-
-    #xcodebuild -exportArchive "./Release-iphoneos/${targetName}.app" -o ~/app.ipa
-fi
-
-
- 
